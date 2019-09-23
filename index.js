5e59da75e4273377d4ee177aca38c829ebaf17f8
xiekind <ziekind07@gmail.com>
2019-09-23 08:35:06 +0800

    added file

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// <!-- BUTA-ANON -->
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+io.on('connection', (socket) => {
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    socket.emit('login');
+  });
+
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+
+
+
+
+
+
+
+
+
+
+
+// var http = require('http');
+
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
